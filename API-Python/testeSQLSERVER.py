a4cd3ae4ea5e3fcb6cfc83133ca417a19808946c
Vinicius-Alves02 <vinicius.vasques@sptech.school>
2022-10-31 14:21:06 -0300

    Atualizacoes

--- API-Python/testeSQLSERVER.py
+++ API-Python/testeSQLSERVER.py
@@ -3,13 +3,13 @@ from lib2to3.pgen2 import driver
 from sqlite3 import Cursor, Row
 import pyodbc
 
-driver = "ODBC Driver 13 for SQL Server"
+driver = "ODBC Driver 18 for SQL Server"
 server = "tcp:hcs-bd.database.windows.net,1433"
 database = "hcs-bd"
 username = "hcs-Grupo09"
 password = "hardwareCSg9"
 
-string_conexao = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:hcs-bd.database.windows.net;Database=hcs-bd;Uid=hcs-Grupo09;Pwd=hardwareCSg9;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+string_conexao = 'Driver={'+ driver +'};'+ 'Server=' + server + ';Database=' + database +';Uid='+ username + ';Pwd='+ password + ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
 conexao = pyodbc.connect(string_conexao)
 cursor = conexao.cursor()
